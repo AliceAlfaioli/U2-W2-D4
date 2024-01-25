8b3e85b8e9e93dfbd5e042ebd2dd440f3041833b
Alice Alfaioli <alicealice5693@yahoo.it>
2024-01-25 16:40:56 +0100

    First commit

---
+++ D4.js
@@ -0,0 +1,130 @@
+/* ESERCIZIO 1
+ Scrivi una funzione di nome "area", che riceve due parametri (l1, l2) e calcola l'area del rettangolo associato.
+*/
+
+function area(l1, l2) {
+  return l1 * l2;
+}
+
+const areaResult = area(10, 5);
+console.log(areaResult);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* ESERCIZIO 2
+ Scrivi una funzione di nome "crazySum", che riceve due numeri interi come parametri.
+ La funzione deve ritornare la somma dei due parametri, ma se il valore dei due parametri è il medesimo deve invece tornare la loro somma moltiplicata per tre.
+*/
+function crazySum(num1, num2) {
+  if (num1 === num2) {
+    return (num1 + num2) * 3;
+  } else {
+    return num1 + num2;
+  }
+}
+
+console.log(crazySum(3, 3));
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* ESERCIZIO 3
+ Scrivi una funzione di nome "crazyDiff" che calcola la differenza assoluta tra un numero fornito come parametro e 19.
+ Deve inoltre tornare la differenza assoluta moltiplicata per tre qualora il numero fornito sia maggiore di 19.
+*/
+
+function crazyDiff(num) {
+  if (num > 19) {
+    return Math.abs(num - 19) * 3;
+  } else {
+    return Math.abs(num - 19);
+  }
+}
+
+console.log(crazyDiff(3));
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* ESERCIZIO 4
+ Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure
+ se n è uguale a 400.
+*/
+
+function boundary(n) {
+  if ((n > 20 && n <= 100) || n === 400) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log("esercizio 4", boundary(3));
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* ESERCIZIO 5
+ Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
+ La funzione deve aggiungere la parola "EPICODE" all'inizio della stringa fornita, ma se la stringa fornita comincia già con "EPICODE" allora deve
+ ritornare la stringa originale senza alterarla.
+*/
+
+function epify(stringa) {
+  if (stringa.startsWith("EPICODE")) {
+    return stringa;
+  } else {
+    return "EPICODE" + stringa;
+  }
+}
+
+console.log(epify("Hello"));
+console.log(epify("EPICODE"));
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* ESERCIZIO 6
+ Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
+ di 3 o di 7. (Suggerimento: usa l'operatore modulo)
+*/
+
+function check3and7(numero) {
+  if (numero % 3 == 0 || numero % 7 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(check3and7(20));
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* ESERCIZIO 7
+ Scrivi una funzione di nome "reverseString", il cui scopo è invertire una stringa fornita come parametro (es. "EPICODE" --> "EDOCIPE")
+*/
+
+function reverseString(str) {
+  return str.split("").reserve("").join("");
+}
+
+console.log(reverseString("EPICODE"));
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* ESERCIZIO 8
+ Scrivi una funzione di nome "upperFirst", che riceve come parametro una stringa formata da diverse parole.
+ La funzione deve rendere maiuscola la prima lettera di ogni parola contenuta nella stringa.
+*/
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* ESERCIZIO 9
+ Scrivi una funzione di nome "cutString", che riceve come parametro una stringa. La funzione deve creare una nuova stringa senza il primo e l'ultimo carattere
+ della stringa originale.
+*/
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* ESERCIZIO 10
+ Scrivi una funzione di nome "giveMeRandom", che accetta come parametro un numero n e ritorna un'array contenente n numeri casuali inclusi tra 0 e 10.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
